ea709e4ff0e330f3eacd3675e1dc2b645d61a026
adrian-helberg <adrianbostelmann@gmx.de>
2019-10-09 23:18:41 +0200

    GKA: Reworked intern data structure

--- GKA/gka/src/de/haw/gka/adtgraph.erl
+++ GKA/gka/src/de/haw/gka/adtgraph.erl
@@ -3,11 +3,16 @@
 %%% @copyright (C) 2019, <HAW Hamburg>
 %%% Created : 26. Sep 2019
 %%% Interne Repräsentation eines (vollständigen) Graphen:
-%%% * nondirected:  [1,2,1,3,1,4,2,3,2,4,3,4]
-%%% * directed:     [1,2,1,3,1,4,2,1,2,3,2,4,3,1,3,2,3,4,4,1,4,2,4,3]
-%%% Zwei aufeinanderfolgende Knoten beschreiben eine Kante
-%%% Auch Einzelne Knoten werden als Kante dargestellt ([1,nil,2,nil])
-%%%   (ein Knoten mit id 1 und ein Knoten mit id 2, keine Kanten)
+%%% * nondirected:  [
+%%%                   {1, ['size', 5, 'capacity', 3]}, {2, ['size', 2]}, {3, []}, (Knoten)
+%%%                   {1, 2, ['weight', 7, 'pass', 1]}, {1, 3, []}, {2, 3, ['weight', 8]} (Kanten)
+%%%                 ]
+%%% * directed:     [
+%%%                   {1, ['size', 5, 'capacity', 3]}, {2, ['size', 2]}, {3, []}, (Knoten)
+%%%                   {1, 2, ['weight', 7, 'pass', 1]}, {1, 3, []}, {2, 3, ['weight', 8]} (Kanten)
+%%%                   {2, 1, ['weight', 1, 'pass', 3]}, {3, 1, []}, {3, 2, ['weight', 2]}
+%%%                 ]
+%%% TODO: directed
 %%%---------------------------------------------------------------------
 -module(adtgraph).
 -author("AdrianHelberg").
@@ -18,44 +23,44 @@ createG(ud) -> util:setglobalvar(yndirected, ud), [];
 createG(_) -> nil.
 
 addVertex(G, V) ->
-  case is_integer(V) of
-    true -> case hasVertex(G, V) of
-              true -> G;
-              false -> G++[V, nil]
-            end;
-    false -> G
+  case hasVertex(G, V) of
+    true -> G;
+    false -> G++[{V, []}]
   end.
 
-% TODO: directed
 deleteVertex([], _) -> [];
-% Vertex is first head and not connected
-deleteVertex([V, nil|T], V) -> deleteVertex(T, V);
-% Vertex is second head and not connected
-deleteVertex([nil, V|T], V) -> deleteVertex(T, V);
-% Vertex is first head and not connected
-deleteVertex([V, W|T], V) -> [nil, W|deleteVertex(T, V)];
-% Vertex is second head and not connected
-deleteVertex([W, V|T], V) -> [W, nil|T];
-% Vertex is neither first, nor second head
+deleteVertex([{V, _}|T], V) -> deleteVertex(T, V);
+deleteVertex([{V, _, _}|T], V) -> deleteVertex(T, V);
+deleteVertex([{_, V, _}|T], V) -> deleteVertex(T, V);
 deleteVertex([H|T], V) -> [H|deleteVertex(T, V)].
 
+addEdge(G, V1, V2) ->
+  case V1 == V2 of
+    true -> G;
+    false -> case hasEdge(G, V1, V2) of
+               true -> G;
+               false -> [addVertex(addVertex(G, V1), V2)|{V1, V2, []}]
+            end
+  end.
+
+deleteEdge([], _, _) -> [];
+deleteEdge([{V1, V2, _}|T], V1, V2) -> deleteEdge(T, V1, V2);
+deleteEdge([{V2, V1, _}|T], V1, V2) -> deleteEdge(T, V1, V2);
+deleteEdge([H|T], V1, V2) -> [H|deleteEdge(T, V1, V2)].
 
 % --- Helper
 hasVertex([], _) -> false;
-hasVertex([V|_], V) -> true;
+hasVertex([{V, _}|_], V) -> true;
 hasVertex([_|T], V) -> hasVertex(T, V).
 
 hasEdge([], _, _) -> false;
-hasEdge([V1, V2|_], V1, V2) -> true;
+hasEdge([{V1, V2, _}|_], V1, V2) -> true;
+hasEdge([{V2, V1, _}|_], V1, V2) -> true;
 hasEdge([_|T], V1, V2) -> hasEdge(T, V1, V2).
-
-findVertex([], _, _) -> nil;
-findVertex([V|_], V, C) -> C;
-findVertex([_|T], V, C) -> findVertex(T, V, C+1).
 
 print(G) ->
   {ok, S} = file:open("graph.dot", [write]),
   io:format(S, "~s~n", [io:format("graph {\n~s\n}", [toString(G)])]),
   os:cmd("dot -Tpng graph.dot -o graph.png").
 
-toString([H1, H2|T]) -> io:format("~b--~b", [H1, H2]).
+toString([H1, H2|_]) -> io:format("~b--~b", [H1, H2]).

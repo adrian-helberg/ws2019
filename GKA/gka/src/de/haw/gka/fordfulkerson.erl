081cc1a6d1a8e112fd7a1745687b4ef428658360
adrian-helberg <adrianbostelmann@gmx.de>
2019-12-05 14:06:48 +0100

    dot to png, initialisierung ford-fulkerson

--- GKA/gka/src/de/haw/gka/fordfulkerson.erl
+++ GKA/gka/src/de/haw/gka/fordfulkerson.erl
@@ -4,5 +4,38 @@
 %%% @copyright (C) 2019, <HAW Hamburg>
 %%%-------------------------------------------------------------------
 -module(fordfulkerson).
+-export([fordfulkerson/3]).
 
-fordfulkerson(Filename, Q, S) -> adtgraph:importG(Filename, d).
+fordfulkerson(Filename, Q, S) ->
+  % Graphen aus Datei einlesen
+  G = adtgraph:importG(Filename, d),
+  %%% INITIALISIERUNG %%%
+  % 1. Weise allen Kanten f(eij) := 0 zu
+  Edges = adtgraph:getEdges(G),
+  G_ = initialize(G, Edges, 'flow', 0),
+  graphToDot(G_, 'test'),
+  dotToPNG('test', tuple_size(G_) * 100),
+  tuple_size(G_).
+
+initialize(G, [], _, _) -> G;
+initialize(G, [H1, H2|T], Name, Value) ->
+  G_ = adtgraph:setAtE(G, H1, H2, Name, Value),
+  initialize(G_, T, Name, Value).
+
+graphToDot(G, Filename) ->
+  File = lists:concat([Filename, '.dot']),
+  % Existierende Datei löschen, da printGFF Dateien nicht überschreibt
+  file:delete(File),
+  adtgraph:printGFF(G, Filename).
+
+fileToDot(Filename) ->
+  G = adtgraph:importG(Filename, d),
+  adtgraph:printGFF(G, 'graph_01').
+
+dotToPNG(Filename, Sleep) ->
+  % Warte bis die dot-Datei geschrieben wurde
+  timer:sleep(Sleep),
+  os:cmd(lists:concat(["dot -Tpng ", Filename , ".dot > ", Filename, ".png"])).
+
+decompile(BeamFileName) ->
+  file:write_file("./decompile", io_lib:fwrite("~p.\\n", [beam_disasm:file(BeamFileName)])).
--- GKA/gka/src/de/haw/gka/test.erl
+++ GKA/gka/src/de/haw/gka/test.erl
@@ -5,15 +5,9 @@
 -module(test).
 -author("Main").
 -include_lib("eunit/include/eunit.hrl").
--compile(export_all).
 
-graphToDot(Filename) ->
-  G = adtgraph:importG(Filename, d),
-  adtgraph:printGFF(G, 'graph_01').
-
-dotToPNG(Filename) ->  os:cmd(lists:concat(["dot -Tpng ", Filename , ".dot > ", Filename, ".png"])).
+-export([test/0]).
 
 test() ->
-  graphToDot('graph_01'),
-  timer:sleep(1000),
-  dotToPNG('graph_01').
+  0.
+

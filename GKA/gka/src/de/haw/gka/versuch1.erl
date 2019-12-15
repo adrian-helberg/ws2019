85b6c7811096d4a914156daffc99e07793813a55
adrian-helberg <adrianbostelmann@gmx.de>
2019-12-15 19:28:40 +0100

    GKA: Presentation fourth version

--- GKA/gka/src/de/haw/gka/versuch1.erl
+++ GKA/gka/src/de/haw/gka/versuch1.erl
@@ -3,17 +3,33 @@
 -export([start/0]).
 
 start() ->
-  test([10,20,30,40,50,60,70,80,90,100]).
+  test2([105,110,115,120,125,130,135,140,145,150]).
+%%  test2([10,20,30,40,50,60,70,80,90,100]).
 
-test([]) -> ok;
-test([VH|VT]) ->
-  gengraph:gengraph(VH,VH-1,VH-1,1,10,'test.graph'),
-  G = adtgraph:importG('test',d),
+test1([]) -> ok;
+test1([VH|VT]) ->
+  gengraph:gengraph(60, 59, 59, 1, VH, 'versuch1.graph'),
+  G = adtgraph:importG('versuch1',d),
   V = lists:sort(adtgraph:getVertices(G)),
   [Q|_] = V,
   [S|_] = lists:reverse(V),
-  erlang:display(['Anzahl Ecken',lists:flatlength(adtgraph:getVertices(G))]),
-  erlang:display(['Anzahl Kanten',lists:flatlength(adtgraph:getEdges(G))]),
+  StartFF = get_timestamp(),
+  fordfulkerson:fordfulkersonT(G, Q, S),
+  EndFF = get_timestamp(),
+
+  StartEK = get_timestamp(),
+  edmondskarp:edmondskarpT(G, Q, S),
+  EndEK = get_timestamp(),
+  erlang:display({'FF', EndFF-StartFF, 'EK', EndEK-StartEK}),
+  test1(VT).
+
+test2([]) -> ok;
+test2([VH|VT]) ->
+  gengraph:gengraph(VH,VH-1,VH-1,1,1,'versuch2.graph'),
+  G = adtgraph:importG('versuch2',d),
+  V = lists:sort(adtgraph:getVertices(G)),
+  [Q|_] = V,
+  [S|_] = lists:reverse(V),
 
   StartFF = get_timestamp(),
   fordfulkerson:fordfulkersonT(G, Q, S),
@@ -23,7 +39,7 @@ test([VH|VT]) ->
   edmondskarp:edmondskarpT(G, Q, S),
   EndEK = get_timestamp(),
   erlang:display({'FF', EndFF-StartFF, 'EK', EndEK-StartEK}),
-  test(VT).
+  test2(VT).
 
 get_timestamp() ->
   {Mega, Sec, Micro} = os:timestamp(),
